a59c6ce2854c1e7c91b3646e9e831178ae70cf60
IkaTheFox <pikafeu.sm@gmail.com>
2017-11-22 14:52:36 +0100

    Created Master

---
+++ ChatClientProject/ChatClientProject/ChatRoom.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProjectS7
+{
+    class ChatRoom
+    {
+        private int roomNumber;
+        static private int roomCount;
+        private List<Chatter> usrList;
+        private string topic;
+        public delegate void Handler(object sender, msg m);
+        public event Handler msgHandler;
+
+        public string Topic
+        {
+            get
+            {
+                return topic;
+            }
+
+            set
+            {
+                topic = value;
+            }
+        }
+
+        protected virtual void onMsgReceive(object sender, msg m)
+        {
+            if (msgHandler != null)
+            {
+                System.Console.WriteLine("Subscription found");
+                msgHandler(sender, m);
+            }
+        }
+
+        public void RaiseMsgReceive(msg Message)
+        {
+            System.Console.WriteLine("Raising msg event");
+            onMsgReceive(this, Message);
+        }
+
+        public ChatRoom()
+        {
+            roomCount++;
+            this.roomNumber = roomCount;
+            usrList = new List<Chatter>();
+            msgHandler += new Handler(msgReceived);
+        }
+
+        public bool join(Chatter joining_user)
+        {
+            foreach (Chatter usr in usrList)
+            {
+                if (usr.Nickname == joining_user.Nickname)
+                {
+                    return false;
+                }
+            }
+            usrList.Add(joining_user);
+            //myDelegateClass = Handler
+            //MyDel = msgHandler
+            //HEREBY subscription
+            return true;
+        }
+
+        public bool quit(Chatter leaving_user)
+        {
+            usrList.Remove(leaving_user);
+            return true;
+        }
+
+        public void msgReceived(object sender, msg m)
+        {
+            System.Console.WriteLine(m.Sender.ToString() + " > " + m.Message);
+        }
+        public void sendMsg(Chatter sender, string message)
+        {
+            msg packet = new msg(sender, message);
+            RaiseMsgReceive(packet);
+        }
+
+    }
+    public class msg : EventArgs
+    {
+        Chatter sender;
+        string message;
+
+        internal Chatter Sender
+        {
+            get
+            {
+                return sender;
+            }
+        }
+
+        public string Message
+        {
+            get
+            {
+                return message;
+            }
+        }
+        public msg(Chatter sender, string message) : base()
+        {
+            this.sender = sender;
+            this.message = message;
+
+        }
+    }
+}
---
+++ ChatClientProject/ChatClientProject/Chatter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectS7
+{
+    public class Chatter
+    {
+        private string nickname;
+        private int auth;
+        private static int number_of_users = 0;
+
+        public string Nickname
+        {
+            get
+            {
+                return nickname;
+            }
+
+            set
+            {
+                nickname = value;
+            }
+        }
+
+        public int Auth
+        {
+            get
+            {
+                return auth;
+            }
+            
+        }
+
+        public Chatter(string nick)
+        {
+            this.Nickname = nick;
+            this.auth = number_of_users;
+            number_of_users++;
+        }
+        public Chatter(int authNumber, string nick)
+        {
+            this.Nickname = nick;
+            this.auth = authNumber;
+        }
+        public string ToString()
+        {
+            return this.Nickname;
+        }
+    }
+}
